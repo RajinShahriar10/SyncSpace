38c1ae83027e230792c8e27d4fd94efe035a35bc
MD. RAJIN SHAHRIAR <shahriarstudios2k@gmail.com>
2026-08-20 15:52:46 +0600

    fix: update tunnel URL, add GitHub OAuth credentials to startup

--- start-backend.ps1
+++ start-backend.ps1
@@ -2,5 +2,7 @@ $env:ConnectionStrings__DefaultConnection = 'Host=ep-bitter-moon-aoefzjzq-pooler
 $env:ASPNETCORE_ENVIRONMENT = 'Development'
 $env:Jwt__Key = 'YourSuperSecretDevelopmentKeyHereThatIsAtLeast32CharactersLong!!'
 $env:Redis__Connection = ''
+$env:GitHub__ClientId = 'Ov23liBN7G6tcfLhKrbl'
+$env:GitHub__ClientSecret = 'a8273f56742e9c02fadb07200cb590f4efb0459b'
 
 dotnet run --project backend/src/SyncSpace.API/SyncSpace.API.csproj --urls 'http://localhost:5000'
